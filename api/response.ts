58723996b126681d5c766f5e502cb51147ec832c
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-04 12:08:20 +0900

    :sparkles: Add type definitions of import data

--- api/response.ts
+++ api/response.ts
@@ -144,9 +144,48 @@ export interface ExportPage<hasMetadata extends true | false = false> {
   lines: hasMetadata extends true ? Omit<Line, "id" | "userId">[]
     : string[];
 }
+
 export interface ExportData<hasMetadata extends true | false = false> {
   /** project's name */ name: string;
   /** project's display name */ displayName: string;
   /** このデータを生成した日時 (UNIX時刻) */ exported: number;
   /** exported pages */ pages: ExportPage<hasMetadata>[];
+}
+
+/** メタデータ無しインポート用ページデータ */
+export interface ImportedLightPage {
+  /** page's title
+   *
+   * `title` should be equal to `lines[0]`
+   */
+  title: string;
+  /** page's text
+   *
+   * `lines[0]` should be equal to `title`
+   */
+  lines: string[];
+}
+/** インポート用メタデータ付き行データ */
+export interface ImportedLine {
+  /** line text */ text: string;
+  /** 行の最終更新日時 (UNIX時刻) */ updated?: number;
+  /** 行の最終作成日時 (UNIX時刻) */ created?: number;
+}
+/** メタデータ付きインポート用ページデータ */
+export interface ImportedPage {
+  /** page's title
+   *
+   * `title` should be equal to `lines[0].text`
+   */
+  title: string;
+  /** page's line data
+   *
+   * `lines[0].text` should be equal to `title`
+   */
+  lines: ImportedLine[];
+}
+/** JSON data for importing by https://scrapbox.io/api/page-data/import/:projectname.json  */
+export interface ImportedData<hasMetadata extends true | false = false> {
+  /** pages importing to a project */
+  pages: hasMetadata extends true ? ImportedPage[] : ImportedLightPage;
 }
