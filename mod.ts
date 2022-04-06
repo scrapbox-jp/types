54815fc38310fd22befc6afb828dbbda01e22471
takker <37929109+takker99@users.noreply.github.com>
2022-04-07 06:15:07 +0900

    :bug: s/error/base

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 export * from "./response.ts";
 export * from "./error.ts";
-export * from "./error.ts";
+export * from "./base.ts";
 export type { Scrapbox } from "./userscript.ts";
 import * as UserScript from "./userscript.ts";
 export { UserScript };
