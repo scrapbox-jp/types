0e002da908faa3b9aa17092d4cb44c43aebe6862
takker99 <37929109+takker99@users.noreply.github.com>
2022-04-07 06:05:30 +0900

    :sparkles: ScrapboxはUserScript.をつけなくても従来どおり使えるようにする

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
 export * from "./response.ts";
 export * from "./error.ts";
+export * from "./error.ts";
+export type { Scrapbox } from "./userscript.ts";
 import * as UserScript from "./userscript.ts";
 export { UserScript };
