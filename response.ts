5ea7094bb812c1aaf800a08070d3bb7ea521fa6e
takker99 <37929109+takker99@users.noreply.github.com>
2024-12-21 17:45:27 +0900

    fix: Catch up with the response type from `https://scrapbox.io/api/page-data/export/:projectname.json`

--- response.ts
+++ response.ts
@@ -301,16 +301,16 @@ export interface SearchedTitle
 
 /** exportしたときのページデータ */
 export interface ExportedPage<hasMetadata extends true | false = false>
-  extends Pick<Page, "title" | "updated" | "created" | "id"> {
+  extends Pick<Page, "title" | "updated" | "created" | "id" | "views"> {
   /** ページ本文
    *
    * `hasMetadata === true`のときは行のmetadataが入る
    * それ以外の場合は行のテキストが入る
    */
-  lines: hasMetadata extends true ? Omit<BaseLine, "id" | "userId">[]
-    : string[];
+  lines: hasMetadata extends true ? Omit<BaseLine, "id">[] : string[];
 }
 
+/** JSON data exported from https://scrapbox.io/api/page-data/export/:projectname.json  */
 export interface ExportedData<hasMetadata extends true | false = false> {
   /** project's name */
   name: string;
@@ -321,9 +321,16 @@ export interface ExportedData<hasMetadata extends true | false = false> {
   /** このデータを生成した日時 (UNIX時刻) */
   exported: UnixTime;
 
+  /** project members */
+  users: UserForExport[];
+
   /** exported pages */
   pages: ExportedPage<hasMetadata>[];
 }
+
+/** user infomation included in exported data */
+export interface UserForExport
+  extends Pick<UserInfo, "id" | "name" | "displayName" | "email"> {}
 
 /** backupされるページデータ */
 export interface BackupedPage
