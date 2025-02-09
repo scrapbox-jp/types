0f2f1b81159abfccd7224af3d4618b3573e1292b
takker99 <37929109+takker99@users.noreply.github.com>
2025-02-09 09:22:02 +0900

    fix(api): Adjust the response definition of /api/project-backup/:projectname/:backupId.json

--- api/project-backup/project/backupId.ts
+++ api/project-backup/project/backupId.ts
@@ -1,17 +1,18 @@
 import type { BaseLine, StringLc, UnixTime } from "../../../base.ts";
+import type { UserForExport } from "../../page-data/export/project.ts";
 import type { Page } from "../../pages/project/title.ts";
 
 /** backupされるページデータ */
 export interface BackupedPage
-  extends Pick<Page, "title" | "updated" | "created" | "id"> {
+  extends Pick<Page, "title" | "updated" | "created" | "id" | "views"> {
   /** ページ本文 */
-  lines: Pick<BaseLine, "text" | "created" | "updated">[];
+  lines: Pick<BaseLine, "text" | "created" | "updated" | "userId">[];
 
   /** ページに含まれているリンク*/
   linksLc: StringLc[];
 }
 
-/** project backup data */
+/** project backup data got from /api/project-backup/:projectname/:backupId.json */
 export interface BackupData {
   /** project's name */
   name: string;
@@ -21,6 +22,9 @@ export interface BackupData {
 
   /** このデータを生成した日時 (UNIX時刻) */
   exported: UnixTime;
+
+  /** project members */
+  users: UserForExport[];
 
   /** backuped pages */
   pages: BackupedPage[];
