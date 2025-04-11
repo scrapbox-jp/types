4e856beab2221c49623fa2d42c1af040c3d52970
takker99 <37929109+takker99@users.noreply.github.com>
2025-04-11 15:50:30 +0900

    style: deno fmt

--- websocket/event.ts
+++ websocket/event.ts
@@ -28,7 +28,7 @@ export interface EmitEventMap {
         | { error: { name: string; message?: string } },
     ) => void,
   ) => void;
-  "page-leave": (req: { projectId: ProjectId, pageId: PageId }) => void;
+  "page-leave": (req: { projectId: ProjectId; pageId: PageId }) => void;
   cursor: (req: Omit<MoveCursorData, "socketId">) => void;
 }
 
@@ -107,15 +107,15 @@ export interface ProjectUpdatesStreamCommit {
   pageId: PageId;
   userId: UserId;
   changes:
-  | (
-    | InsertChange
-    | UpdateChange
-    | DeleteChange
-    | TitleChange
-    | LinksChange
-    | IconsChange
-  )[]
-  | [DeletePageChange];
+    | (
+      | InsertChange
+      | UpdateChange
+      | DeleteChange
+      | TitleChange
+      | LinksChange
+      | IconsChange
+    )[]
+    | [DeletePageChange];
   cursor: null;
   freeze: true;
 }
@@ -126,8 +126,8 @@ export interface CommitNotification extends PageCommit {
 
 export interface QuickSearchCommit extends Omit<CommitNotification, "changes"> {
   changes:
-  | (TitleChange | LinksChange | DescriptionsChange | ImageChange)[]
-  | [DeletePageChange];
+    | (TitleChange | LinksChange | DescriptionsChange | ImageChange)[]
+    | [DeletePageChange];
 }
 
 export interface QuickSearchReplaceLink {
